e3d73384235ad84288c0ce9d0646c294831b8f97
Maicius <maicius@outlook.com>
2019-12-26 01:56:26 +0800

    更新入口

--- entry.py
+++ entry.py
@@ -1,6 +1,6 @@
 from ConvertImage import ConvertImage
 import argparse
-
+from time import sleep
 parser = argparse.ArgumentParser()
 parser.add_argument("-image_dir", type=str, default="image/")
 parser.add_argument('-result_dir', type=str, default="image/")
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     批处理程序入口
     """
     args = parser.parse_args()
+    print("***************** 照片墙一键生成器*********************")
+    print("*******************制作：小麦冬***********************")
     print("原始照片地址：", args.image_dir)
     print("转换后照片地址：", args.result_dir)
     print("照片额外信息地址：", args.photo_info)
@@ -19,7 +21,7 @@ if __name__ == '__main__':
     ci = ConvertImage()
     ci.do_convert_image(path=args.image_dir, new_path=args.result_dir, request_base_dir=args.request_dir,
                         photo_info=args.photo_info)
-    print("批处理照片完成，请用浏览器打开PhotoWall.html即可查看照片墙")
-    print("按任意键离开程序...")
-    input()
+    print("恭喜您，批处理照片完成！")
+    print("请稍等5秒，若浏览器未自动打开，请手动使用浏览器打开PhotoWall.html...")
+    sleep(5)
 
