f3fb618d68c5b4aa548c80291901f2581be5ebf7
Maicius <maicius@outlook.com>
2019-12-26 00:49:55 +0800

    完善批处理入口

--- entry.py
+++ entry.py
@@ -1,4 +1,4 @@
-from src.ConvertImage import ConvertImage
+from ConvertImage import ConvertImage
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -6,12 +6,20 @@ parser.add_argument("-image_dir", type=str, default="image/")
 parser.add_argument('-result_dir', type=str, default="image/")
 parser.add_argument("-request_dir", type=str, default="image/")
 parser.add_argument("-photo_info", type=str, default="photo_info.txt")
+
 if __name__ == '__main__':
     """
     批处理程序入口
     """
     args = parser.parse_args()
-    print(args.image_dir)
+    print("原始照片地址：", args.image_dir)
+    print("转换后照片地址：", args.result_dir)
+    print("照片额外信息地址：", args.photo_info)
+    print("照片请求地址(在本地浏览无需修改此项)：", args.request_dir)
     ci = ConvertImage()
     ci.do_convert_image(path=args.image_dir, new_path=args.result_dir, request_base_dir=args.request_dir,
-                        image_info=args.photo_info)
+                        photo_info=args.photo_info)
+    print("批处理照片完成，请用浏览器打开PhotoWall.html即可查看照片墙")
+    print("按任意键离开程序...")
+    input()
+
